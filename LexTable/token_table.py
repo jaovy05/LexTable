61960f17165d0190ba27f654cb74c8a0e635830f
Gomes <joaovitorg.silva050@gmail.com>
2025-04-21 22:23:41 -0300

    feat: handle conflicts when creating table and print transitions alphabetically

--- LexTable/token_table.py
+++ LexTable/token_table.py
@@ -2,17 +2,11 @@ import numpy as np
 
 def create_table(tokens):
     alphabet = make_alphabet(tokens)
-    transitions = [
-        {symbol: -1 for symbol in alphabet} 
-        for _ in range(len(alphabet))
-    ]
-
-    last_state = 0
+    transitions = [{symbol: -1 for symbol in alphabet}]
 
     for token in tokens:
-        last_state = make_token_table(token, transitions, last_state, alphabet)
-    
-    transitions.append(make_empty_transition(alphabet))
+        make_token_table_recursive(token, transitions, alphabet)
+        transitions.append(make_empty_transition(alphabet))
 
     print_transitions(transitions, alphabet)
     return 1
@@ -21,24 +15,67 @@ def create_table(tokens):
 def make_alphabet(tokens) -> set[str]:
     return {char for token in tokens for char in token}
     
-def make_token_table(token, transitions, last_state, alphabet) -> int:
-    i = last_state + 1
-    transitions[0][token[0]] = i 
-
-    for char in token[1:]:
-        if i == len(transitions):
-            transitions.append(make_empty_transition(alphabet))
-
-        transitions[i][char] = i + 1
-        i += 1
+# def make_token_table(token, transitions, last_state, alphabet) -> int:
+#     current = last_state + 1
     
-    return i
+#     if transitions[0][token[0]] == -1:
+#         transitions[0][token[0]] = current
+#         i = 1
+#     else:
+#         transitions[transitions[0][token[0]]][token[1]] = current
+#         i = 2
 
+#     n = len(token)
+
+#     while i < n:
+#         char = token[i]
+
+#         if current == len(transitions):
+#             transitions.append(make_empty_transition(alphabet))
+
+#         if transitions[current][char] == -1:
+#             transitions[current][char] = current + 1
+#         else:
+#             transitions[transitions[current][char]][token[i + 1]] = current + 1
+#             i += 1
+
+#         current += 1
+#         i += 1
+#     return current
+
+def make_token_table_recursive(token: str, transitions, alphabet) ->  tuple[int, bool]:
+    if len(token) == 1:
+        last_pos = len(transitions)
+        if transitions[0][token] == -1:
+            transitions[0][token] = last_pos
+            return last_pos, False
+        else:
+            return 0, True
+        
+    i, conflito = make_token_table_recursive(token[:-1], transitions, alphabet) 
+
+    if conflito:
+        lenT = len(transitions) 
+        i = transitions[i][token[-2]]
+        if transitions[i][token[-1]] == -1:
+            transitions[i][token[-1]] = lenT
+            return lenT, False
+        else:
+            return i, True
+        
+    else:
+        transitions.append(make_empty_transition(alphabet))
+        if transitions[i][token[-1]] == -1:
+            transitions[i][token[-1]] = i + 1
+            return i + 1, False
+        else:
+            return transitions[i][token[-1]], True
 
 def make_empty_transition(alphabet: set[str]) -> dict[str, int]:
     return {char: -1 for char in alphabet}
 
 def print_transitions(transitions, alphabet, show_errors=False):
+    alphabet = sorted(alphabet)   
     print("    ", end="")
     for symbol in alphabet:
         print(f"{symbol:^5}", end="") 
@@ -55,7 +92,7 @@ def print_transitions(transitions, alphabet, show_errors=False):
         print()
 
 def main():
-    tokens = ["ao", "e", "iua", "xw", "trz"]
+    tokens = ["aete", "aeca","fogofatua", "fogofat"]
     create_table(tokens)
 
 if __name__ == "__main__":
